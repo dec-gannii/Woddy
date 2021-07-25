ab8187d9e4aa54f114e3fd7b401493495c520a4b
SungJihyun <jhsung23@gmail.com>
2021-07-25 22:38:01 +0900

    login

--- app/src/main/java/com/example/woody/LogInActivity.java
+++ app/src/main/java/com/example/woody/LogInActivity.java
@@ -3,12 +3,20 @@ package com.example.woody;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LogInActivity extends AppCompatActivity {
+    EditText idEditText;
+    EditText pwEditText;
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        idEditText = findViewById(R.id.id_edit_text);
+        pwEditText = findViewById(R.id.password_edit_text);
+        loginButton = findViewById(R.id.login_button);
     }
 }
